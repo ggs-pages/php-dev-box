f5697e293fd3cbe5bce0c7fba9c49dc021d47cdf
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2016-06-29 10:16:58 +0200

    Added docker compose

--- vagrant/default/docker.sh
+++ vagrant/default/docker.sh
@@ -18,3 +18,7 @@ sudo service docker start
 
 # sudo groupadd docker
 sudo usermod -aG docker vagrant
+
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /tmp/docker-compose
+sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
